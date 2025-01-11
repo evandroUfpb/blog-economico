e7bdb0802a6bc6228f53d6edfc75f67424cde483
Evandro Farias <evdrocha@gmail.com>
2025-01-11 16:39:51 -0300

    Restaurando arquivos ajustes v_14

--- app/diagnose.py
+++ app/diagnose.py
@@ -8,17 +8,21 @@ def diagnose():
     print(f"Platform: {platform.platform()}")
     print(f"Python Implementation: {platform.python_implementation()}")
     
-    try:
-        import numpy
-        print(f"NumPy Version: {numpy.__version__}")
-    except ImportError:
-        print("NumPy não instalado")
+    modules_to_check = [
+        'numpy', 
+        'pandas', 
+        'flask', 
+        'sidrapy', 
+        'requests', 
+        'plotly'
+    ]
     
-    try:
-        import pandas
-        print(f"Pandas Version: {pandas.__version__}")
-    except ImportError:
-        print("Pandas não instalado")
+    for module in modules_to_check:
+        try:
+            mod = __import__(module)
+            print(f"{module.capitalize()} Version: {mod.__version__}")
+        except ImportError:
+            print(f"{module.capitalize()} não instalado")
 
 if __name__ == "__main__":
     diagnose()
